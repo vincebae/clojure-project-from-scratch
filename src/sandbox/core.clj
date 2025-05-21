8f6a02e2e83e4aedcc9fc5a0e4dc53fd15c731a4
Seung-Bin Bae <vincebae@gmail.com>
2025-05-20 23:41:00 -0700

    Initial project setup

---
+++ src/sandbox/core.clj
@@ -0,0 +1,11 @@
+(ns sandbox.core
+  (:require [clojure.string :as s])
+  (:gen-class))
+
+(defn greet [xs]
+  (if (empty? xs)
+    "Hello, World!"
+    (str "Hello, " (s/join ", " xs) "!")))
+
+(defn -main [& args]
+  (println (greet args)))
