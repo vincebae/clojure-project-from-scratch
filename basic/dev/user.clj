c80d5ef33eda30d59275827daae6996b36870d2e
Seung-Bin Bae <vincebae@gmail.com>
2025-07-09 23:05:26 -0700

    update user.clj

--- basic/dev/user.clj
+++ basic/dev/user.clj
@@ -1,18 +1,18 @@
 (ns user
-  "Scratch pad for dev")
+  "Scratch pad for dev"
+  (:require [clj-commons.pretty.repl :as p]
+            [clojure.core.async :as a]
+            [clojure.java.io :as io]
+            [clojure.pprint :refer [pprint]]
+            [clojure.string :as s]
+            [clojure.test :as test]
+            [dev :refer [system go! stop! refresh!]]))
 
-(require '[clj-commons.pretty.repl :as p]
-         '[clojure.core.async :as a]
-         '[clojure.java.io :as io]
-         '[clojure.pprint :refer [pprint]]
-         '[clojure.string :as s]
-         '[clojure.test :as test]
-         '[dev :refer [system go! stop! refresh!]])
-
-@system
-(go!)
-(go! "Alice" "Bob")
-(stop!)
-(refresh!)
-(refresh! "Alice" "Bob")
+#(
+  @system
+  (go!)
+  (go! "Alice" "Bob")
+  (stop!)
+  (refresh!)
+  (refresh! "Alice" "Bob"))
 
